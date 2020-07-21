2e954673ffcc78348130cc6838fa9f92ce68452b
pawan1773 <35738777+pawan1773@users.noreply.github.com>
2020-07-22 01:43:52 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const FILE_CONFIG = [{
-	"url": "https://documentcloud.adobe.com/view-sdk-demo/PDFs/Bodea Brochure.pdf",
+	"url": "https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwi909akid_qAhUNcCsKHVX-C7AQFjAAegQIAxAB&url=https%3A%2F%2Fwww.w3.org%2FWAI%2FER%2Ftests%2Fxhtml%2Ftestfiles%2Fresources%2Fpdf%2Fdummy.pdf&usg=AOvVaw1yfXcABf-Bej4cjTs8tPJn",
 	"fileName": "PERSONAL_INFORMATION.pdf",
 	"fileCategory": "PERSONAL INFORMATION"
 }, {
@@ -15,7 +15,7 @@ const FILE_CONFIG = [{
 	"fileName": "POLICE_CLEARANCE.pdf",
 	"fileCategory": "POLICE CLEARANCE"
 }, {
-	"url": "https://github.com/pawan1773/ePass/blob/master/pdf/samplePdfFromHtml.pdf",
+	"url": "https://pawan1773.github.io/ePass/pdf/samplePdfFromHtml.pdf",
 	"fileName": "SELF_DECLARATION.pdf",
 	"fileCategory": "SELF DECLARATION"
 }];
