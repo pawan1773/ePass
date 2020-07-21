29f33ac2337e889299f5e22649d27718ed1e9288
pawan1773 <35738777+pawan1773@users.noreply.github.com>
2020-07-22 01:52:36 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,21 +1,21 @@
 const FILE_CONFIG = [{
-	"url": "https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwi909akid_qAhUNcCsKHVX-C7AQFjAAegQIAxAB&url=https%3A%2F%2Fwww.w3.org%2FWAI%2FER%2Ftests%2Fxhtml%2Ftestfiles%2Fresources%2Fpdf%2Fdummy.pdf&usg=AOvVaw1yfXcABf-Bej4cjTs8tPJn",
+	"url": "https://pawan1773.github.io/ePass/pdf/personal.pdf",
 	"fileName": "PERSONAL_INFORMATION.pdf",
 	"fileCategory": "PERSONAL INFORMATION"
 }, {
-	"url": "https://documentcloud.adobe.com/view-sdk-demo/PDFs/Bodea Brochure.pdf",
+	"url": "https://pawan1773.github.io/ePass/pdf/vehile.pdf",
 	"fileName": "VEHICLE_DETAILS.pdf",
 	"fileCategory": "VEHICLE DETAILS"
 }, {
-	"url": "https://documentcloud.adobe.com/view-sdk-demo/PDFs/Bodea Brochure.pdf",
+	"url": "https://pawan1773.github.io/ePass/pdf/medical.pdf",
 	"fileName": "MEDICAL_CERTIFICATE.pdf",
 	"fileCategory": "MEDICAL CERTIFICATE"
 }, {
-	"url": "https://documentcloud.adobe.com/view-sdk-demo/PDFs/Bodea Brochure.pdf",
+	"url": "https://pawan1773.github.io/ePass/pdf/pcc.pdf",
 	"fileName": "POLICE_CLEARANCE.pdf",
 	"fileCategory": "POLICE CLEARANCE"
 }, {
-	"url": "https://pawan1773.github.io/ePass/pdf/samplePdfFromHtml.pdf",
+	"url": "https://pawan1773.github.io/ePass/pdf/declaration.pdf",
 	"fileName": "SELF_DECLARATION.pdf",
 	"fileCategory": "SELF DECLARATION"
 }];
