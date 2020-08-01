9c875aa1f8355567aecf6535684ddc6e8ea0d28c
pawan1773 <35738777+pawan1773@users.noreply.github.com>
2020-08-01 11:35:38 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -2,22 +2,27 @@
 const domain = 'https://' + location.hostname + '/ePass/pdf/';
 
 const FILE_CONFIG = [{
+	"id": 1,
 	"url": domain + "personal.pdf",
 	"fileName": "PERSONAL_INFORMATION.pdf",
 	"fileCategory": "PERSONAL INFORMATION"
 }, {
+	"id": 2,
 	"url": domain + "vehicle.pdf",
 	"fileName": "VEHICLE_DETAILS.pdf",
 	"fileCategory": "VEHICLE DETAILS"
 }, {
+	"id": 3,
 	"url": domain + "medical.pdf",
 	"fileName": "MEDICAL_CERTIFICATE.pdf",
 	"fileCategory": "MEDICAL CERTIFICATE"
 }, {
+	"id": 4,
 	"url": domain + "pcc.pdf",
 	"fileName": "POLICE_CLEARANCE.pdf",
 	"fileCategory": "POLICE CLEARANCE"
 }, {
+	"id": 5,
 	"url": domain + "declaration.pdf",
 	"fileName": "SELF_DECLARATION.pdf",
 	"fileCategory": "SELF DECLARATION"
